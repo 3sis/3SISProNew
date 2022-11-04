2f6030c8afab8876e28128f77fe181b65ee3b393
krishna <krishnawaghmode1995@gmail.com>
2022-11-04 21:02:50 +0530

    first commit

--- clear3SIS.bat
+++
@@ -1,5 +0,0 @@
-php artisan cache:clear
-php artisan route:cache
-php artisan view:clear
-php artisan config:cache
-php artisan serve --port=8000
--- index.php
+++ index.php
@@ -1,21 +1,124 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Home </title>
+    <link rel="icon" type="image/x-icon" href="public/assets/img/favicon.ico"/>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/plugins.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
+    <style>
+        .border{
+          border: 1px solid #f0f;
+          background: #F5821F;
+        }
+         body.maintanence > .maintanence-content {
+            min-height: 107vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            padding: 205px;
+        }
+        a{
+          color: #fff;
+        }
+    </style>
 
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
-);
+</head>
+<body class="maintanence text-center">
+    
+    <div class="header-container fixed-top">
+        <header class="header navbar navbar-expand-sm">
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
-}
+            <ul class="navbar-item theme-brand flex-row  text-center">
+                <li class="nav-item theme-logo">
+                    <a href="index.html">
+                        <img src="img/logo.svg" class="navbar-logo" alt="logo">
+                    </a>
+                </li>
+                <li class="nav-item theme-text">
+                    <a href="index.html" class="nav-link"> CORK </a>
+                </li>
+            </ul>
+           
+        </header>
+    </div>
 
-require_once __DIR__.'/public/index.php';
+    <div class="container-fluid maintanence-content">
+
+       <div class="row">
+  <div class="col-sm-6">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">S&OP Pro</h5> -->
+        <h4><a href="http://localhost/project/laravel_demo/login" >Payroll</a></h4>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Fiexed Asset </h5> -->
+        <h4><a href="http://localhost/project/project2" >Fiexed Asset</a></h4>
+      </div>
+    </div>
+  </div>
+   <div class="col-sm-6 mt-2">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Payroll</h5> -->
+        <h4><a href="http://localhost/test/test_2">S&OP Pro</a></h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 mt-2">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Sales Force Automation</h5> -->
+        <h4><a href="http://localhost/test/test_4" >Sales Force Automation</a></h4>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 mt-2">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Sales Force Automation</h5> -->
+        <h4><a href="https://app.powerbi.com" target="_blank" >BI</a></h4>
+      </div>
+    </div>
+  </div>
+   <div class="col-sm-6 mt-2">
+    <div class="card border">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Sales Force Automation</h5> -->
+        <h4><a href="http://localhost/test/test_4" >AR/AP</a></h4>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 mt-2">
+    <div class="card border bg-success">
+      <div class="card-body">
+        <!-- <h5 class="card-title">Sales Force Automation</h5> -->
+        <h4><a href="http://localhost/test/test_4" >Tutorial</a></h4>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+</body>
+</html>
